29338919b03ab6f53c72eafaebaa801e7ecd4fec
Spiderllama <72757628+Spiderllama@users.noreply.github.com>
2021-09-30 18:58:03 -0400

    Add project files.

---
+++ BiggestOf3/BiggestOf3.Designer.cs
@@ -0,0 +1,148 @@
+﻿
+namespace BiggestOf3
+{
+    partial class BiggestOf3
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Yellow;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Font = new System.Drawing.Font("Stencil", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(69, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(329, 85);
+            this.label1.TabIndex = 0;
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.Yellow;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label2.Font = new System.Drawing.Font("Stencil", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(69, 120);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(329, 85);
+            this.label2.TabIndex = 1;
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.Yellow;
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label3.Font = new System.Drawing.Font("Stencil", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(69, 219);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(329, 85);
+            this.label3.TabIndex = 2;
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Times New Roman", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(119, 331);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(216, 69);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Next";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Times New Roman", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button2.Location = new System.Drawing.Point(419, 331);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(216, 69);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Check It";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label4
+            // 
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label4.Font = new System.Drawing.Font("Rockwell", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(419, 33);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(369, 172);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Click Next to generate 3 random numbers. ";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label5
+            // 
+            this.label5.Font = new System.Drawing.Font("Rockwell", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(436, 116);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(334, 84);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Click Check It to mark the largest.";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // BiggestOf3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "BiggestOf3";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.BiggestOf3_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ BiggestOf3/BiggestOf3.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BiggestOf3
+{
+    public partial class BiggestOf3 : Form
+    {
+        public BiggestOf3()
+        {
+            InitializeComponent();
+        }
+
+        private void BiggestOf3_Load(object sender, EventArgs e)
+        {
+            label1.BackColor = Color.Red;
+            label2.BackColor = Color.Red;
+            label3.BackColor = Color.Red;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+         //Generate a random number and assign it to all 3 slots on click
+            Random RNG = new Random();
+
+            label1.Text = RNG.Next(1, 101).ToString();
+            label2.Text = RNG.Next(1, 101).ToString();
+            label3.Text = RNG.Next(1, 101).ToString();
+
+            //Reset the colors to RED
+            label1.BackColor = Color.Red;
+            label2.BackColor = Color.Red;
+            label3.BackColor = Color.Red;
+
+            //set Check It to active button
+            button2.Select();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+            int LABEL1;
+            int LABEL2;
+            int LABEL3;
+
+            //Read the RNG numbers from the Text property and assign it to local INTS
+            LABEL1 = Convert.ToInt16(label1.Text);
+            LABEL2 = Convert.ToInt16(label2.Text);
+            LABEL3 = Convert.ToInt16(label3.Text);
+
+            //Test which number is bigger by comparing to each other. Change the color of the background to blue for biggest or multiple if matching numbers.
+            if (LABEL1.CompareTo(LABEL2) >= 0 & LABEL1.CompareTo(LABEL3) >= 0)
+            {
+                label1.BackColor = Color.Blue;
+            }
+            if (LABEL2.CompareTo(LABEL1) >= 0 & LABEL2.CompareTo(LABEL3) >= 0)
+            {
+                label2.BackColor = Color.Blue;
+            }
+            if (LABEL3.CompareTo(LABEL1) >= 0 & LABEL3.CompareTo(LABEL2) >= 0)
+            {
+                label3.BackColor = Color.Blue;
+            }
+            //set Next button as active
+            button1.Select();
+        }
+    }
+}
